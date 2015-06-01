4dd044e0f8ce1abb7a579e2793bdb41d7b0258f9
ctranxuan <ctranxuan@gmail.com>
2015-06-01 22:28:17 +0200

    feat(polling sample): add refresh button + cosmetic
    - add refresh button to refresh the graphs
    - remove some console.log(.)
    - add a nanobar when voting

--- app/options/option.controller.js
+++ app/options/option.controller.js
@@ -9,27 +9,33 @@
       var questionId = $routeParams.questionId;
 
       if (!$rootScope.currentQuestion) {
-        QuestionService.get({'where': angular.toJson({objectId: questionId})}).$promise.then(function (result) {
-          $rootScope.currentQuestion = result;
-          vm.question = $rootScope.currentQuestion;
-        });
+        QuestionService.get({'where': angular.toJson({objectId: questionId})}).$promise
+          .then(function (result) {
+            $rootScope.currentQuestion = result;
+            vm.question = $rootScope.currentQuestion;
+          });
 
       } else {
         vm.question = $rootScope.currentQuestion;
 
       }
 
-
-
-      OptionService.get({'where': angular.toJson({question_id: questionId})}).$promise.then(function (result) {
-        vm.options = result;
-      });
+      OptionService.get({'where': angular.toJson({question_id: questionId})}).$promise
+        .then(function (result) {
+          vm.options = result;
+        });
     };
 
     vm.vote = function () {
-      console.log('-------------- vote!' + vm.question.objectId + " " + $scope.selectedOptionId);
+      var nanobar = new Nanobar({
+        bg: "#27ae60"
+      });
+      nanobar.go(30);
 
-      ResultService.save({"question_id": vm.question.objectId, "option_id":  $scope.selectedOptionId});
+      ResultService.save({"question_id": vm.question.objectId, "option_id":  $scope.selectedOptionId}).$promise
+        .then(function() {
+          nanobar.go(100);
+        });
 
     };
 
--- app/results/result.controller.js
+++ app/results/result.controller.js
@@ -6,7 +6,6 @@
        var vm = this;
 
        $scope.ctx = $('canvas')[0].getContext("2d");
-       var context = $scope.ctx;
        $scope.chartData = [];
        $scope.chartOptions = {
          responsive: true,
@@ -14,50 +13,61 @@
          animationSteps: 45
        };
 
-       vm.init  = function() {
-         var questionId = $routeParams.questionId;
+       function createChart(votes) {
+         var colors = [];
+         var total = 0;
 
-         if (!$rootScope.currentQuestion) {
-           QuestionService.get({'where': angular.toJson({objectId: questionId})}).$promise.then(function (result) {
-             $rootScope.currentQuestion = result;
-             vm.question = $rootScope.currentQuestion;
-           });
-
-         } else {
-           vm.question = $rootScope.currentQuestion;
+         if ($scope.chartData.length !== votes.length) {
+           colors = color.randomColors(votes.length);
+           $scope.chartData = $.extend(true, votes, colors);
 
          }
 
-         ResultService.getPoll(questionId).then(function (data) {
-           vm.results = data;
+         total = votes.reduce(function(previousValue, currentValue) {
+           return previousValue + currentValue.value;
+         }, total);
 
-           var colors = [];
-           var total = 0;
+         if (total > 0) {
+           $scope.chart = new Chart($scope.ctx).Doughnut($scope.chartData, $scope.chartOptions);
 
-           if ($scope.chartData.length !== data.length) {
-             colors = color.randomColors(data.length);
-             $scope.chartData = $.extend(true, data, colors);
+         }
+       };
 
-           } else {
-             $scope.chartData = $.extend(true, $scope.chartData, data);
+       function updateChart(votes) {
+         $scope.chartData = $.extend(true, $scope.chartData, votes);
 
+         if ($scope.chart != null) {
+           for(var i = 0; i < votes.length; i++) {
+             $scope.chart.segments[i].value = votes[i].value;
            }
 
-           total = data.reduce(function(previousValue, currentValue) {
-             return previousValue + currentValue.value;
-           }, total);
+           $scope.chart.update();
+         }
+       };
 
-           if ($scope.chart != null) {
-             // TODO update the chart
-             for(var i = 0; i < data.length; i++) {
-               $scope.chart.segments[i].value = data[i].value;
-             }
+       vm.init  = function() {
+         var questionId = $routeParams.questionId;
 
-             $scope.chart.update();
-           } else if (total > 0) {
-            $scope.chart = new Chart($scope.ctx).Doughnut($scope.chartData, $scope.chartOptions);
+         if ($rootScope.currentQuestion) {
+           vm.question = $rootScope.currentQuestion;
 
-           }
+         } else {
+           QuestionService.get({'where': angular.toJson({objectId: questionId})}).$promise
+             .then(function (result) {
+                $rootScope.currentQuestion = result;
+                vm.question = $rootScope.currentQuestion;
+            });
+
+         }
+
+         ResultService.getPoll(questionId).then(function (votes) {
+           createChart(votes);
+         });
+       };
+
+       vm.refresh = function() {
+         ResultService.getPoll(vm.question.objectId).then(function (votes) {
+           updateChart(votes);
          });
        };
 
