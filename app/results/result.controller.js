74dd2d878146606bf75f81040eeda9ead8c5259c
ctranxuan <ctranxuan@gmail.com>
2015-06-02 15:10:09 +0200

    feat(streaming sample): refactor code to be more readable and use the same function as the polling sample

--- app/results/result.controller.js
+++ app/results/result.controller.js
@@ -2,11 +2,10 @@
 
 (function() {
 
-     function ResultController($scope, $rootScope, $routeParams, QuestionService, OptionService, StreamingResultService) {
+     function ResultController($scope, $rootScope, $routeParams, QuestionService, StreamingResultService) {
        var vm = this;
 
        $scope.ctx = $('canvas')[0].getContext("2d");
-       var context = $scope.ctx;
        $scope.chartData = [];
        $scope.chartOptions = {
          responsive: true,
@@ -14,23 +13,38 @@
          animationSteps: 45
        };
 
-       vm.updateChartData = function(data, options) {
-         var optionsMap = {};
-         options.forEach(function (option) {
-           optionsMap[option.objectId] = {label: option.Title, value: 0};
-         });
+       function createChart(votes) {
+         var colors = [];
+         var total = 0;
 
-         data.results.forEach(function (vote) {
-           optionsMap[vote.option_id].value++;
-         });
+         if ($scope.chartData.length !== votes.length) {
+           colors = color.randomColors(votes.length);
+           $scope.chartData = $.extend(true, votes, colors);
 
-         var results = [];
-         for (var option in optionsMap) {
-           results.push(optionsMap[option]);
          }
 
-         return results;
-       }
+         total = votes.reduce(function(previousValue, currentValue) {
+           return previousValue + currentValue.value;
+         }, total);
+
+         if (total > 0) {
+           $scope.chart = new Chart($scope.ctx).Doughnut($scope.chartData, $scope.chartOptions);
+           $scope.$digest();
+         }
+       };
+
+       function updateChart(votes) {
+         $scope.chartData = $.extend(true, $scope.chartData, votes);
+
+         if ($scope.chart != null) {
+           for(var i = 0; i < votes.length; i++) {
+             $scope.chart.segments[i].value = votes[i].value;
+           }
+
+           $scope.chart.update();
+           $scope.$digest();
+         }
+       };
 
        vm.init  = function() {
          var questionId = $routeParams.questionId;
@@ -46,88 +60,10 @@
 
          }
 
-
-         OptionService.get({'where': angular.toJson({question_id: questionId})}).$promise.then(function(options) {
-           vm.options = options;
-
-           var url = 'https://api.parse.com/1/classes/Vote?where=' + encodeURIComponent('{\"question_id\": \"' + questionId + '\"}');
-           var streamdata = StreamingResultService.init(url);
-
-           streamdata.onData(function(data) {
-             var results = vm.updateChartData(data, options);
-             vm.results = data;
-             var colors = [];
-             var total = 0;
-
-             if ($scope.chartData.length !== results.length) {
-               colors = color.randomColors(results.length);
-               $scope.chartData = $.extend(true, results, colors);
-
-             } else {
-               $scope.chartData = $.extend(true, $scope.chartData, results);
-
-             }
-
-             //total = data.reduce(function(previousValue, currentValue) {
-             //  return previousValue + currentValue.value;
-             //}, total);
-             console.log('****-------**' + JSON.stringify(results));
-             console.log('**----------**' + JSON.stringify($scope.chartData));
-             $scope.chart = new Chart($scope.ctx).Doughnut($scope.chartData, $scope.chartOptions);
-             $scope.$digest();
-
-           })
-             .onPatch(function(patch) {
-               jsonpatch.apply(vm.results, patch);
-               var results = vm.updateChartData(vm.results, vm.options);
-
-               for(var i = 0; i < results.length; i++) {
-                 $scope.chart.segments[i].value = results[i].value;
-               }
-               $scope.chartData = $.extend(true, $scope.chartData, results);
-               $scope.chart.update();
-               $scope.$digest();
-
-             })
-             .onError(function(error) {
-               console.error('--------' + error);
-             });
-
-
-           ;
+         var streamdataPromise = StreamingResultService.getPoll(questionId, createChart, updateChart);
+         streamdataPromise.then(function(streamdata) {
            streamdata.open();
          });
-       //  ResultService.getPoll(questionId).then(function (data) {
-       //    vm.results = data;
-       //
-       //    var colors = [];
-       //    var total = 0;
-       //
-       //    if ($scope.chartData.length !== data.length) {
-       //      colors = color.randomColors(data.length);
-       //      $scope.chartData = $.extend(true, data, colors);
-       //
-       //    } else {
-       //      $scope.chartData = $.extend(true, $scope.chartData, data);
-       //
-       //    }
-       //
-       //    total = data.reduce(function(previousValue, currentValue) {
-       //      return previousValue + currentValue.value;
-       //    }, total);
-       //
-       //    if ($scope.chart != null) {
-       //      // TODO update the chart
-       //      for(var i = 0; i < data.length; i++) {
-       //        $scope.chart.segments[i].value = data[i].value;
-       //      }
-       //
-       //      $scope.chart.update();
-       //    } else if (total > 0) {
-       //     $scope.chart = new Chart($scope.ctx).Doughnut($scope.chartData, $scope.chartOptions);
-       //
-       //    }
-       //  });
        };
 
        vm.init();
@@ -136,6 +72,6 @@
 
  angular
    .module('pollApp')
-   .controller('ResultController', ['$scope', '$rootScope', '$routeParams', 'QuestionService', 'OptionService', 'StreamingResultService', ResultController]);
+   .controller('ResultController', ['$scope', '$rootScope', '$routeParams', 'QuestionService', 'StreamingResultService', ResultController]);
 
  })();
--- app/results/result.streaming.service.js
+++ app/results/result.streaming.service.js
@@ -1,29 +1,72 @@
 'use strict';
 
-(function() {
+(function () {
 
-  function StreamingResultService(appConfig) {
+  function StreamingResultService(appConfig, $q, OptionService) {
+    function mapToVotes(votesResultSet, options) {
+
+      var optionsMap = {};
+      options.forEach(function (option) {
+        optionsMap[option.objectId] = {label: option.Title, value: 0};
+      });
+
+      votesResultSet.results.forEach(function (vote) {
+        optionsMap[vote.option_id].value++;
+      });
+
+      var votes = [];
+      for (var option in optionsMap) {
+        votes.push(optionsMap[option]);
+      }
+
+      return votes;
+    }
+
     return {
-      init: function(url) {
-        // you can store your key pair in a json file instead, more details in documentation
-        streamdataio.Pk = appConfig.streamdataioAppToken;
-        streamdataio.pk = appConfig.streamdataioPrivateKey;
+      getPoll: function (questionId, onDataCallback, onPatchCallback) {
+        var deferred = $q.defer();
+        var param = {question_id: questionId};
+        var jsonParam = angular.toJson(param);
 
-        var headers = ['X-Parse-Application-ID: ' + appConfig.parseAppId, 'X-Parse-REST-API-Key:' + appConfig.parseRestApiId];
-        // create the Streamdata source
-        var streamdata = streamdataio.createEventSource(url, headers);
+        OptionService.get({'where': jsonParam}).$promise.then(function (options) {
+          var url = 'https://api.parse.com/1/classes/Vote?where=' + encodeURIComponent('{\"question_id\": \"' + questionId + '\"}');
 
-        streamdata.streamdataConfig.PROTOCOL = 'http://';
-        streamdata.streamdataConfig.HOST = 'proxy.streamdata.io';
-        streamdata.streamdataConfig.PORT = '9080';
+          // you can store your key pair in a json file instead, more details in documentation
+          streamdataio.Pk = appConfig.streamdataioAppToken;
+          streamdataio.pk = appConfig.streamdataioPrivateKey;
 
-        return streamdata;
+          var headers = ['X-Parse-Application-ID: ' + appConfig.parseAppId, 'X-Parse-REST-API-Key:' + appConfig.parseRestApiId];
+          // create the streamdata.io source
+          var streamdata = streamdataio.createEventSource(url, headers);
+
+          streamdata.streamdataConfig.PROTOCOL = 'http://';
+          streamdata.streamdataConfig.HOST = 'proxy.streamdata.io';
+          streamdata.streamdataConfig.PORT = '9080';
+
+          streamdata.onData(function (data) {
+            streamdata.resultSet = data;
+            var votes = mapToVotes(data, options);
+
+            onDataCallback(votes);
+
+          }).onPatch(function (patch) {
+            jsonpatch.apply(streamdata.resultSet, patch);
+            var votes = mapToVotes(streamdata.resultSet, options);
+
+            onPatchCallback(votes);
+          });
+
+          deferred.resolve(streamdata);
+
+        });
+
+        return deferred.promise;
       }
     }
   }
 
   angular
     .module('pollApp')
-    .factory('StreamingResultService', ['appConfig', StreamingResultService]);
+    .factory('StreamingResultService', ['appConfig', '$q', 'OptionService', StreamingResultService]);
 
 }());
