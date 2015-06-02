708e9fb45564e20da532e8f8fefc7d0183bdeb71
ctranxuan <ctranxuan@gmail.com>
2015-06-02 16:25:01 +0200

    style(streaming): code cleanup

--- app.controller.js
+++ app.controller.js
@@ -1,20 +1,12 @@
 'use strict';
 
-(function() {
+(function () {
 
-     function PollAppController($scope, $log, $timeout) {
-       var vm = this;
+  function PollAppController() {
+  };
 
-       vm.init  = function() {
+  angular
+    .module('pollApp')
+    .controller('PollAppController', [PollAppController]);
 
-       };
-
-       vm.init();
-
-     };
-
- angular
-   .module('pollApp')
-   .controller('PollAppController', ['$scope', '$log', '$timeout', PollAppController]);
-
- })();
+})();
