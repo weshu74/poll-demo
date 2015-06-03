6a27c75d26474d0a93db155673770882ef48c1b8
ctranxuan <ctranxuan@gmail.com>
2015-06-03 10:49:48 +0200

    doc(streaming): added comments to the code

--- app/results/result.streaming.service.js
+++ app/results/result.streaming.service.js
@@ -40,17 +40,24 @@
           var streamdata = streamdataio.createEventSource(url, headers);
 
           streamdata.onData(function (data) {
+            // add a callback when data is sent by streamdata.io: it's the first set of data sent (snapshot)
             streamdata.resultSet = data;
             var votes = mapToVotes(data, options);
 
             onDataCallback(votes);
 
           }).onPatch(function (patch) {
+            // a callback when a patch is sent by streamdata.io: whenever the snapshot has changed, streamdata.io sent the diff
             jsonpatch.apply(streamdata.resultSet, patch);
             var votes = mapToVotes(streamdata.resultSet, options);
 
             onPatchCallback(votes);
+
+          }).onError(function(error) {
+            console.error(error);
+
           });
+
 
           deferred.resolve(streamdata);
 
